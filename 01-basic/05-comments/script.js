7a0e1eee938372e80e3af33374a8d458fb1ced99
Priyal Patel <priyal230906@gmail.com>
2026-08-29 23:14:16 +0530

    Comments in JavaScript

---
+++ 01-basic/05-comments/script.js
@@ -0,0 +1,17 @@
+// This script greets the user based on the current time.
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+// Display appropriate greeting based on the current time.
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
